f505b02b05ddba0f23d7126ed22f8d095191e302
Denis Moyogo Jacquerye <moyogo@gmail.com>
2020-02-05 09:16:47 +0000

    Add build.sh

---
+++ sources/build.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+set -e
+
+# Go the sources directory to run commands
+SOURCE="${BASH_SOURCE[0]}"
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+cd $DIR
+echo $(pwd)
+
+echo "Generating Static fonts"
+mkdir -p ../fonts
+fontmake -m RobotoMono.designspace -i -o ttf --output-dir ../fonts/ttf/
+fontmake -m RobotoMono.designspace -i -o otf --output-dir ../fonts/otf/
+
+echo "Generating VFs"
+mkdir -p ../fonts/vf
+fontmake -m RobotoMono.designspace -o variable --output-path ../fonts/vf/RobotoMono[wght].ttf
+
+rm -rf master_ufo/ instance_ufo/ instance_ufos/
+
+
+echo "Post processing"
+ttfs=$(ls ../fonts/ttf/*.ttf)
+for ttf in $ttfs
+do
+	gftools fix-dsig -f $ttf;
+done
+
+vfs=$(ls ../fonts/vf/*\[wght\].ttf)
+
+echo "Post processing VFs"
+for vf in $vfs
+do
+	gftools fix-dsig -f $vf;
+done
+
+
+
+echo "Fixing VF Meta"
+gftools fix-vf-meta $vfs;
+
+echo "Dropping MVAR"
+for vf in $vfs
+do
+	mv "$vf.fix" $vf;
+	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
+	rtrip=$(basename -s .ttf $vf)
+	new_file=../fonts/vf/$rtrip.ttx;
+	rm $vf;
+	ttx $new_file
+	rm $new_file
+done
+
+echo "Fixing Hinting"
+for vf in $vfs
+do
+	gftools fix-nonhinting $vf $vf.fix;
+	mv "$vf.fix" $vf;
+done
+for ttf in $ttfs
+do
+	gftools fix-nonhinting $ttf $ttf.fix;
+	mv "$ttf.fix" $ttf;
+done
+
+rm ../fonts/ttf/*gasp.ttf ../fonts/vf/*gasp.ttf
+
